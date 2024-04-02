fbd7a07863b408d0186a9973306ccf97aebda30d
Luis M <lmarti32@uccs.edu>
2024-04-01 22:29:34 -0600

    Fixed bugs with cow. Updated Ring Around The Rosie

--- Assets/Scripts/CowCollisionDetection.cs
+++ Assets/Scripts/CowCollisionDetection.cs
@@ -21,20 +21,15 @@ public class CowCollisionDetection : MonoBehaviour
 
     private void Update()
     {
-        if (!sphereCollider.enabled)
+        if (!sphereCollider.enabled && closestApple != null)
         {
             closestApple = null;
             Destroy(gemApple);
         }
-        else if (closestApple != null && closestApple.transform.parent != null)
+        else if (closestApple != null && closestApple.transform.parent != null && closestApple.transform.parent.tag.Contains("Player"))
         {
             agent.SetDestination(transform.position);
             closestApple = null;
-            Destroy(gemApple);
-        }
-        else if (agent.isActiveAndEnabled && closestApple == null && agent.destination != transform.position)
-        {
-            agent.SetDestination(transform.position);
             Destroy(gemApple);
         }
         else if (gemApple != null && inventory.hasApple && !inventory.inventorySlots[inventory.currentInventorySlot, 0].name.Contains("Apple") && closestApple == null)
